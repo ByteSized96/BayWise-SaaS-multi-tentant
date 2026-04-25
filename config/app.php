fb0de33b5e34a95f606482dde664eb0ef9c74eaa
Codex96 <83451111+ByteSized96@users.noreply.github.com>
2026-04-25 01:05:22 +0100

    Add files via upload

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'app_name' => 'BayWise Portal',
+    'app_url' => 'http://localhost/public',
+
+    'db' => [
+        'host' => 'localhost',
+        'name' => '--',
+        'user' => '--',
+        'pass' => '--',
+    ],
+];
+?>
